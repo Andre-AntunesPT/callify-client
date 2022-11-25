77ce535b36595e82c3733bdc698bf3456f029a42
Raiza Garcia <raizagarcias@gmail.com>
2022-11-25 15:18:06 +0000

    add anon and private

---
+++ src/components/Private.jsx
@@ -0,0 +1,23 @@
+import {useContext} from 'react';
+import {AuthContext} from '../contexts/auth.context';
+import {Navigate} from 'react-router-dom';
+
+
+function Private(props) {
+    const {loading, loggedIn} = useContext(AuthContext);
+    
+    //first we check if the page is still loading
+    if(loading) return <p>Loading...</p>
+   
+    //if the user in not logged in we redirect to the login page
+    if (!loggedIn){
+        return <Navigate to='/login'/>
+   
+        //if the user is logged in we return the children(the page we are trying to protect)    
+     } else {
+        return props.children
+    }
+
+}
+
+export default Private;
