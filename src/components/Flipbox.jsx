d990a2cc9f52d751a56cad17472c4159a1483b3e
André Antunes <andre.n.antunes@gmail.com>
2022-11-28 11:46:38 +0000

    flipbox changes

--- src/components/Flipbox.jsx
+++ src/components/Flipbox.jsx
@@ -22,6 +22,60 @@ function Flipbox() {
             <div className="flip-overlay"></div>
           </div>
         </div>
+        <div class="square-flip">
+          <div className="square">
+            <div className="square-container">
+              <h2 className="textshadow">Lorem Ipsum</h2>
+              <h3 className="textshadow">
+                Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum{" "}
+              </h3>
+            </div>
+            <div className="flip-overlay"></div>
+          </div>
+          <div className="square2">
+            <div className="square-container2">
+              <div className="align-center"></div>
+              <h2>Lorem Ipsum Lorem Ipsum Lorem Ipsum </h2>
+            </div>
+            <div className="flip-overlay"></div>
+          </div>
+        </div>
+        <div class="square-flip">
+          <div className="square">
+            <div className="square-container">
+              <h2 className="textshadow">Lorem Ipsum</h2>
+              <h3 className="textshadow">
+                Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum{" "}
+              </h3>
+            </div>
+            <div className="flip-overlay"></div>
+          </div>
+          <div className="square2">
+            <div className="square-container2">
+              <div className="align-center"></div>
+              <h2>Lorem Ipsum Lorem Ipsum Lorem Ipsum </h2>
+            </div>
+            <div className="flip-overlay"></div>
+          </div>
+        </div>
+        <div class="square-flip">
+          <div className="square">
+            <div className="square-container">
+              <h2 className="textshadow">Lorem Ipsum</h2>
+              <h3 className="textshadow">
+                Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum{" "}
+              </h3>
+            </div>
+            <div className="flip-overlay"></div>
+          </div>
+          <div className="square2">
+            <div className="square-container2">
+              <div className="align-center"></div>
+              <h2>Lorem Ipsum Lorem Ipsum Lorem Ipsum </h2>
+            </div>
+            <div className="flip-overlay"></div>
+          </div>
+        </div>
       </div>
     </>
   );
