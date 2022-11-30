aa4da2f64ae70142dab0cb4b0eedd0d41ce69aba
André Antunes <andre.n.antunes@gmail.com>
2022-11-30 00:15:58 +0000

    form label and input design

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,65 +1,95 @@
-import {useState, useContext} from 'react';
-import axios from 'axios';
-import { Link, useNavigate } from 'react-router-dom';
-import {AuthContext} from '../contexts/auth.context';
+import { useState, useContext } from "react";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../contexts/auth.context";
 
 function Login(props) {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [username, setUsername] = useState('');
-    const [errorMessage, setErrorMessage] = useState(undefined);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [errorMessage, setErrorMessage] = useState(undefined);
 
-    const navigate = useNavigate();
-    const {storeToken, authenticateUser} = useContext(AuthContext);
-  
-    const handleEmail = (e) => setEmail(e.target.value);
-    const handlePassword = (e) => setPassword(e.target.value);
-    const handleUsername = (e) => setUsername(e.target.value);
-    
-  
-    const handleLoginSubmit = async(e) => {e.preventDefault();
-        
-        try {
-            //try to create the user
-            const response = await axios.post(`${process.env.REACT_APP_API_URL}/login`, { email, password, username });
-            
-            //store the token thet we get from the login request
-            storeToken(response.data.authToken);
-    
-            //validate the token
-            authenticateUser();
-    
-            //redirect
-            navigate("/")
-        } catch (error) {
-            const errorDescription = error.response.data.message;
-            setErrorMessage(errorDescription);
-        }
+  const navigate = useNavigate();
+  const { storeToken, authenticateUser } = useContext(AuthContext);
 
+  const handleEmail = (e) => setEmail(e.target.value);
+  const handlePassword = (e) => setPassword(e.target.value);
+  const handleUsername = (e) => setUsername(e.target.value);
+
+  const handleLoginSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      //try to create the user
+      const response = await axios.post(
+        `${process.env.REACT_APP_API_URL}/login`,
+        { email, password, username }
+      );
+
+      //store the token thet we get from the login request
+      storeToken(response.data.authToken);
+
+      //validate the token
+      authenticateUser();
+
+      //redirect
+      navigate("/");
+    } catch (error) {
+      const errorDescription = error.response.data.message;
+      setErrorMessage(errorDescription);
     }
-    return (
-    <div className='LoginPage'>
-    <h1>Login</h1>
+  };
+  return (
+    <div className="LoginPage">
+      <h1>Login</h1>
 
-        <form onSubmit={handleLoginSubmit}>
-            <label>Email:</label>
-            <input type='email' name='email' value={email} onChange={handleEmail} />
+      <form onSubmit={handleLoginSubmit}>
+        <div class="conteudo">
+          <div class="meu-box">
+            <input
+              class="input-nome"
+              type="email"
+              name="email"
+              value={email}
+              onChange={handleEmail}
+              placeholder="Email:"
+            />
+            <label class="label-nome">Email</label>
+          </div>
 
-        <label>Password:</label>
-        <input type="password" name="password" value={password} onChange={handlePassword} />
+          <div class="meu-box">
+            <input
+              class="input-nome"
+              type="password"
+              name="password"
+              value={password}
+              onChange={handlePassword}
+              placeholder="Password:"
+            />
+            <label class="label-nome">Password</label>
+          </div>
 
-        <label>Username</label>
-        <input type='text' name='username' value={username} onChange={handleUsername} />
+          <div class="meu-box">
+            <input
+              class="input-nome"
+              type="text"
+              name="username"
+              value={username}
+              onChange={handleUsername}
+              placeholder="Username:"
+            />
+            <label class="label-nome">Username</label>
+          </div>
+        </div>
+        <button type="submit">Login</button>
+      </form>
 
-        <button type='submit'>Login</button>
-        </form>
+      {errorMessage && <p className="error-message">{errorMessage}</p>}
 
-    {errorMessage && <p className="error-message">{errorMessage}</p>}
-
-    <p>Don't have an account yet?</p>
-    <Link to='/login'> Signup! </Link>
+      <p>Don't have an account yet?</p>
+      <Link to="/login"> Signup! </Link>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
